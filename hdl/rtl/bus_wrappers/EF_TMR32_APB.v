7550840ce2147eb56654b625e9be27390e6802c6
ef-staff <ef-staff@users.noreply.github.com>
2025-02-20 11:45:40 +0000

    run bus_wrap(9c68c7c) on EF_TMR32(767f7b0)

--- hdl/rtl/bus_wrappers/EF_TMR32_APB.v
+++ hdl/rtl/bus_wrappers/EF_TMR32_APB.v
@@ -1,5 +1,5 @@
 /*
-	Copyright 2024 Efabless Corp.
+	Copyright 2025 Efabless Corp.
 
 	Author: Efabless Corp. (ip_admin@efabless.com)
 
@@ -95,7 +95,7 @@ module EF_TMR32_APB #(
   wire [ 32-1:0] tmr;
   wire [  1-1:0] matchx_flag;
   wire [  1-1:0] matchy_flag;
-  (* keep *)wire [  1-1:0] timeout_flag;
+  wire [  1-1:0] timeout_flag;
 
   // Register Definitions
   wire [ 32-1:0] TMR_WIRE;
